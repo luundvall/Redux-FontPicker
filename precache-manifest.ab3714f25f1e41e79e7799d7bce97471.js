c749d05ef10a3cfbce33cf8a683b167e4c2aecd2
Karl Lundvall <Karl.Lundvall@evry.com>
2019-01-31 11:17:02 +0100

    Updates

---
+++ precache-manifest.ab3714f25f1e41e79e7799d7bce97471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a3ccbd90336a07f02cf",
+    "url": "/Redux-FontPicker/static/js/runtime~main.8a3ccbd9.js"
+  },
+  {
+    "revision": "6489f400537cda9c2451",
+    "url": "/Redux-FontPicker/static/js/main.6489f400.chunk.js"
+  },
+  {
+    "revision": "6244b0227494a2ad85e3",
+    "url": "/Redux-FontPicker/static/js/1.6244b022.chunk.js"
+  },
+  {
+    "revision": "6489f400537cda9c2451",
+    "url": "/Redux-FontPicker/static/css/main.681c2e18.chunk.css"
+  },
+  {
+    "revision": "6244b0227494a2ad85e3",
+    "url": "/Redux-FontPicker/static/css/1.72e69ecf.chunk.css"
+  },
+  {
+    "revision": "f7e851a7a6bf2d57862af68eb43809b1",
+    "url": "/Redux-FontPicker/index.html"
+  }
+];
